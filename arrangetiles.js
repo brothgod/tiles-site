11a4224b7d21b4a9268af3e810bd1585dc45e219
Mahesh Menon <menontmahesh@gmail.com>
2024-10-09 22:09:48 -0400

    updated tile algorithm and added it to index page

--- arrangetiles.js
+++ arrangetiles.js
@@ -5,17 +5,13 @@ var tileCols = 45;
 var tileRows = 30;
 
 // change color combo
-var color1 = "#002626"; // main color
+var color1 = "black"; // main color
 var color2 = "white"; // background color
 var color3 = "#95C623"; // to make certain shapes specific color
 var tiles;
 
 function setup() {
   //set canvas to any size
-  //createCanvas(600, 400);
-  // createCanvas(800, 400);
-  createCanvas(1000, 1000);
-
   background(color2);
   tileCanvasWidth = 600;
   tileCanvasHeight = 400;
@@ -24,10 +20,14 @@ function setup() {
   tileXStart = 0;
   tileYStart = 0;
 
+  var canvas = createCanvas(tileCanvasWidth, tileCanvasHeight);
+  canvas.parent("canvas");
+  setupBaseTiles();
+
   let mySelect;
 
   mySelect = createSelect();
-  mySelect.position(700, 80 + 1000);
+  mySelect.position(325, 410);
 
   //mySelect.option("5 x 5");
   mySelect.option("15 x 10");
@@ -38,21 +38,18 @@ function setup() {
 
   let myPicker;
   picker1 = createColorPicker(color1);
-  picker1.position(700, 1000);
+  picker1.position(125, 410);
   let c1 = picker1.color();
   picker2 = createColorPicker(color2);
-  picker2.position(700, 40 + 1000);
+  picker2.position(225, 410);
   let c2 = picker2.color();
   color2 = c2;
 
-  let button = createButton("draw");
-  button.position(700, 120 + 1000);
+  let button = createButton("new");
+  button.position(425, 410);
 
   button.mousePressed(() => {
     let e = mySelect.selected();
-
-    console.log(e == "25 x 25");
-
     if (e == "15 x 10") {
       tileCols = 15;
       tileRows = 10;
@@ -70,37 +67,13 @@ function setup() {
 
     color1 = picker1.color();
     color2 = picker2.color();
-
+    setupBaseTiles();
     drawTiles();
   });
 
   tiles = arrangeWithConstraints();
   drawTiles();
 }
-
-function mousePressed() {
-  // e = mySelect.selected();
-}
-
-// function draw() {
-//   let e = mySelect.selected();
-
-//   console.log(e == "25 x 25");
-
-//   if (e == "5 x 5") {
-//     tilesCols = 5;
-//     tilesRows = 5;
-//   } else if (e == "15 x 15") {
-//     tilesCols = 15;
-//     tilesRows = 15;
-//   } else if (e == "25 x 25") {
-//     tilesCols = 25;
-//     tilesRows = 25;
-//   } else if (e == "35 x 35") {
-//     tilesCol = 35;
-//     tilesRow = 35;
-//   }
-// }
 
 function arrangeWithConstraints() {
   console.log("Making new grid of size:", tileRows, tileCols);
@@ -114,8 +87,6 @@ function arrangeWithConstraints() {
   }
 
   const down = new Set([4, 5, 6, 7, 9, 11, 13, 15]);
-  const up = new Set([1, 3, 5, 7, 12, 13, 14, 15]);
-  const left = new Set([8, 9, 10, 11, 12, 13, 14, 15]);
   const right = new Set([2, 3, 6, 7, 10, 11, 14, 15]);
 
   for (var row = 0; row < tileRows; row++) {
@@ -151,13 +122,31 @@ function arrangeWithConstraints() {
         }
         var rand = floor(Math.random() * possibleTiles.length);
         tileThis = possibleTiles[rand];
-        //console.log(row, col, tileThis);
         arr[row][col] = tileThis;
       }
     }
   }
   return arr;
 }
+
+const tileFunctions = [
+  tile0,
+  tile1,
+  tile2,
+  tile3,
+  tile4,
+  tile5,
+  tile6,
+  tile7,
+  tile8,
+  tile9,
+  tile10,
+  tile11,
+  tile12,
+  tile13,
+  tile14,
+  tile15,
+];
 
 function drawTiles() {
   console.log("Redrawing tiles now with size {} {}", tileRows, tileCols);
@@ -170,41 +159,7 @@ function drawTiles() {
       var sx = tileW;
       var sy = tileH;
 
-      // console.log(x, y, tile);
-
-      if (tile == 0) {
-        tile0(x, y, sx, sy);
-      } else if (tile == 1) {
-        tile1(x, y, sx, sy);
-      } else if (tile == 2) {
-        tile2(x, y, sx, sy);
-      } else if (tile == 3) {
-        tile3(x, y, sx, sy);
-      } else if (tile == 4) {
-        tile4(x, y, sx, sy);
-      } else if (tile == 5) {
-        tile5(x, y, sx, sy);
-      } else if (tile == 6) {
-        tile6(x, y, sx, sy);
-      } else if (tile == 7) {
-        tile7(x, y, sx, sy);
-      } else if (tile == 8) {
-        tile8(x, y, sx, sy);
-      } else if (tile == 9) {
-        tile9(x, y, sx, sy);
-      } else if (tile == 10) {
-        tile10(x, y, sx, sy);
-      } else if (tile == 11) {
-        tile11(x, y, sx, sy);
-      } else if (tile == 12) {
-        tile12(x, y, sx, sy);
-      } else if (tile == 13) {
-        tile13(x, y, sx, sy);
-      } else if (tile == 14) {
-        tile14(x, y, sx, sy);
-      } else if (tile == 15) {
-        tile15(x, y, sx, sy);
-      }
+      tileFunctions[tile](x, y, sx, sy);
     }
   }
   console.log("Finished drawing");
